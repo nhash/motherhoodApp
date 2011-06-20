031d4eb20e660a7ddbe26a0ee6a030edb815340c
Nada <nadahashem@gmail.com>
2011-06-20 15:37:41 -0700

    Ruby on Rails generated structure

---
+++ motherhoodApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotherhoodApp::Application.config.secret_token = '0d5f8894e1f9845893e7cb21ea999a3abdcb4e8f0c1eec71bb49c6a8c94bf9af6973c66c76843ff611489a8f695618f6cf618f2d81109b9c0c7bcdff940fcec5'
