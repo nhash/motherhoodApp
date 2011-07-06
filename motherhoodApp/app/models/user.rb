65951b254d47d0fc890597aa6be399b197e9613d
Nada <nadahashem@gmail.com>
2011-07-06 15:09:34 -0700

    user model

--- motherhoodApp/app/models/user.rb
+++ motherhoodApp/app/models/user.rb
@@ -1,13 +1,27 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'digest/sha2'
+require 'rubygems'
+#require 'date_validator'
 
 class User < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
+  validates :password, :confirmation => true
+#  validates :date_of_birth, :presence => true, :date => { :before => Date.today }
+  validates :email, :presence => true, :uniqueness => true#, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+#  validates :g_y_account_name, :uniqueness => true #google or youtube account name
+#  validates :g_y_account_pwd,  :presence => { :if => :g_y_account_name? } #google or youtube account password
   
-  validates :password, :confirmation => true #one password field, and one for the re-type password confirmation
-  attr_accessor :password_confirmation
-  attr_reader :password
-  
-  validate :password_must_be_present
+  attr_accessor :password_confirmation, :email 
+  attr_reader   :password, :email #, :g_y_account_name, :g_y_account_pwd
+
+  validate  :password_must_be_present
 
   def User.authenticate(name, password)
     if user = find_by_name(name)
@@ -16,29 +30,28 @@ class User < ActiveRecord::Base
       end
     end
   end
-  
-  def User.encrypt_password(password, salt)
-    Digest::SHA2.hexdigest(password + "manamam" + salt) #string = first letter of each of our first names =P
-  end  
 
-  #'password' is a virtual atribute
-  #creates a salt and sets the hashed password
+  def User.encrypt_password(password, salt)
+    Digest::SHA2.hexdigest(password + "wibble" + salt)
+  end
+  
+  # 'password' is a virtual attribute
   def password=(password)
     @password = password
-    
+
     if password.present?
       generate_salt
-      self.hashed_password = self.class.encrypt_password(salt)
+      self.hashed_password = self.class.encrypt_password(password, salt)
     end
   end
-
-  private    
   
+  private
+
     def password_must_be_present
-        errors.add(:password, "Missing password") unless hashed_password.present?
+      errors.add(:password, "Missing password") unless hashed_password.present?
     end
-    
-    def generate_salt 
-        self.salt = self.object_id.to_s + rand.to_s #doing self.salt tells ruby that  its not a local variable
+  
+    def generate_salt
+      self.salt = self.object_id.to_s + rand.to_s
     end
 end
