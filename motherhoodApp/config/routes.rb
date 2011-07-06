e817dbc3950d141b856bf70986ca232d0fab9835
Nada <nadahashem@gmail.com>
2011-07-06 15:14:42 -0700

    config settings changed

--- motherhoodApp/config/routes.rb
+++ motherhoodApp/config/routes.rb
@@ -1,4 +1,16 @@
 MotherhoodApp::Application.routes.draw do
+  resources :profiles
+
+  get "admin/index"
+
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
